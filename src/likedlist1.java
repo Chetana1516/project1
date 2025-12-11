32c4f3eb7f5c91e296976948c00c797aa17c6c48
chetana1504 <chetanatemkar2001@gmail.com>
2025-12-11 10:23:29 +0530

    first commit

--- src/likedlist1.java
+++ src/likedlist1.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 public class likedlist1 {
     public static void main(String[] args) {
         LinkedHashSet<Integer> n = new LinkedHashSet<>();
+
         n.add(4);
         n.add(8);
         n.add(3);
